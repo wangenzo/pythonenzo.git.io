534ea01e737c15282eeb6674fdc2d3b5cf2c5c52
wangenzo <iworking_1024@163.com>
2019-05-27 11:22:52 +0800

    2019.05.27-11.22

--- remote_backup.py
+++ remote_backup.py
@@ -3,9 +3,6 @@ import pexpect
 user='root'
 host='192.168.114.58'
 password='000000'
-#sourcedir=' /root/python '
-#destdir='/root/backup/python/'
-#command='scp '+sourcedir +user+'@'+'host'+':'+destdir
 command = 'sudo scp /root/python/* '+user+'@'+host+':/root/bakcup/python/'
 child=pexpect.spawn(command,timeout=10)
 child.expect('password:')
